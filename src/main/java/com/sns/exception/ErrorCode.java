395c01bb8a7ce566b479bc492eacaf2631202a98
KSH <sh7824@naver.com>
2023-04-27 21:31:47 +0900

    ErrorCode , toStream
    에러 코드를 직관적으로 보여주는 toStream 작성

--- src/main/java/com/sns/exception/ErrorCode.java
+++ src/main/java/com/sns/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT,"User name is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "user not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR")
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid")
     ;
 
     private HttpStatus status;
