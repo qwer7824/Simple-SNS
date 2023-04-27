47f513df460a98e65743d9e4bce49e4d54f68bcf
KSH <sh7824@naver.com>
2023-04-27 21:29:34 +0900

    JWT key Setting

--- src/main/java/com/sns/configuration/AuthenticationConfig.java
+++ src/main/java/com/sns/configuration/AuthenticationConfig.java
@@ -1,16 +1,27 @@
 package com.sns.configuration;
 
+import com.sns.configuration.filter.JwtTokenFilter;
+import com.sns.exception.CustomAuthenticationEntryPoint;
+import com.sns.service.UserService;
+import com.sns.util.JwtTokenUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserService userService;
+    @Value("${jwt.secret-key}")
+    private String key;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,8 +32,10 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenFilter(key,userService), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
                 ;
-               // .exceptionHandling()
-                //.authenticationEntryPoint()
     }
 }
