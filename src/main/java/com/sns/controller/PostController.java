91668b50cdb5d16c79d8027432f5653d4f967fe6
KSH <sh7824@naver.com>
2023-05-03 21:32:37 +0900

    Delete 구현
    `Controller`에서 `DeleteMapping` 사용하고 리턴은 Void로 딱히 필요없어서 `success` 값을 리턴하였다.
    `Service`에서는 검증은 동일하게 적용하고 `delete`를 써주었다.

--- src/main/java/com/sns/controller/PostController.java
+++ src/main/java/com/sns/controller/PostController.java
@@ -29,6 +29,11 @@ public class PostController {
         Post post = postService.modify(request.getTitle(), request.getBody(), authentication.getName(), postId);
         return Response.success(PostResponse.fromPost(post));
     }
+    @DeleteMapping("/{postId}")
+    public Response<Void> delete(@PathVariable Integer postId, Authentication authentication){
+        postService.delete(authentication.getName(),postId);
+        return Response.success();
+    }
 
 }
 
