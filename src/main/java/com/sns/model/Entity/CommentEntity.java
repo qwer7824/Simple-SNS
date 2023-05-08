c81eab85f6a6193110c54270f1f9f38d3edb7b4f
KSH <sh7824@naver.com>
2023-05-08 22:01:35 +0900

    Comment Entity(index add)
    `post_id`를 인덱스를 걸어서 성능을 높인다.
    `select * from pg_indexes WHERE tablename='comment'` 를 통해서 `post_id_idx` 잡히는걸 확인

---
+++ src/main/java/com/sns/model/Entity/CommentEntity.java
@@ -0,0 +1,63 @@
+package com.sns.model.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "\"comment\"",indexes = {
+        @Index(name = "post_id_idx", columnList = "post_id")
+})
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE \"comment\" SET deleted_at = NOW() where id=?")
+@Where(clause = "deleted_at is NULL")
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "register_at")
+    private Timestamp registerAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deleted_At;
+
+    @PrePersist
+    void registeredAt(){
+        this.registerAt = Timestamp.from(Instant.now());
+    }
+    @PreUpdate
+    void updatedAt(){
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static CommentEntity of(UserEntity user, PostEntity postEntity,String comment){
+        CommentEntity entity = new CommentEntity();
+        entity.setUser(user);
+        entity.setPost(postEntity);
+        entity.setComment(comment);
+        return entity;
+
+    }
+
+}
