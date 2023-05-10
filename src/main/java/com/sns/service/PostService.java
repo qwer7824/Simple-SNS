4ef768608255410fce1d2edfebae3bf00269f669
KSH <sh7824@naver.com>
2023-05-10 22:46:00 +0900

    #1 - countByPost refactoring
    JPA에서 count*** 같은걸 제공한다. `@Query` 어노테이션을 사용해도 되지만 이렇게 써도 무방하기 때문에 수정한다.

--- src/main/java/com/sns/service/PostService.java
+++ src/main/java/com/sns/service/PostService.java
@@ -55,6 +55,8 @@ public class PostService {
             if (postEntity.getUser() != userEntity) {
                 throw new SnsApplicationException(ErrorCode.INVALID_PERMISSION, String.format("%s has no permission with %s", userName, postId));
             }
+            likeEntityRepository.deleteAllByPost(postEntity);
+            commentEntityRepository.deleteAllByPost(postEntity);
             postEntityRepository.delete(postEntity);
     }
 
@@ -84,7 +86,7 @@ public class PostService {
     }
 
     @Transactional
-    public int likeCount(Integer postId){
+    public long likeCount(Integer postId){
         PostEntity postEntity = getPostEntityOrException(postId);
 
         //List<LikeEntity> likeEntities = likeEntityRepository.findByPost(postEntity);
