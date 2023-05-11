4a315edc598666d7cb7ef0cb8ead0cce5099323a
KSH <sh7824@naver.com>
2023-05-11 23:11:19 +0900

    #3 - User Caching
    `User` 를 Json 으로 변환을 해서 Redis에 넣는 형태가 되는데 불필요한 코드가 생길거 같아서 `username`으로 통일함
    불필요한 `getUsername()` 제거 하였고 Redis에 불필요한 데이터는 `@JsonIgnore` 처리하였음
    서비스 로그인 부분에서 UserEntity를 사용했지만 이제는 필요하지않아 변경하였음.

--- src/main/java/com/sns/service/UserService.java
+++ src/main/java/com/sns/service/UserService.java
@@ -6,6 +6,7 @@ import com.sns.model.Alarm;
 import com.sns.model.Entity.UserEntity;
 import com.sns.model.User;
 import com.sns.repository.AlarmEntityRepository;
+import com.sns.repository.UserCacheRepository;
 import com.sns.repository.UserEntityRepository;
 import com.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,7 @@ public class UserService {
     private final AlarmEntityRepository alarmEntityRepository;
     private final UserEntityRepository userEntityRepository;
     private final BCryptPasswordEncoder encoder;
+    private final UserCacheRepository userCacheRepository;
 
     @Value("${jwt.secret-key}")
     private String secretKey;
@@ -33,8 +35,10 @@ public class UserService {
     private Long expiredTimesMs;
 
     public User loadUserByUserName(String userName){
-        return userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(() ->
-                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        return userCacheRepository.getUser(userName).orElseGet(()->
+        userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)))
+        );
 
     }
 
@@ -54,10 +58,12 @@ public class UserService {
 public String login(String userName , String password){
 
         // 회원가입 여부 체크
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded",userName)));
+        User user = loadUserByUserName(userName);
+
+        userCacheRepository.setUser(user);
 
         // 비밀번호 체크
-    if(!encoder.matches(password,userEntity.getPassword())){
+    if(!encoder.matches(password,user.getPassword())){
             throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
